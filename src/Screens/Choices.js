4c6bc06e7a0ef4f72b2e83d746d5afaa02902d03
Phillip Temple <phillipcorteztemple@gmail.com>
2018-12-30 23:02:33 -0600

    Adding css to buttons

--- src/Screens/Choices.js
+++ src/Screens/Choices.js
@@ -1,10 +1,18 @@
 export default function Choices(){
     return `
         <div class="choices">
-            <button>first</button>
-            <button>second</button>
-            <button>third</button>
-            <button>fourth</button>
+            <button class="History">Histroy</button>
+            <button class="underErasure">Under Erasure</button>
+            <button class="three">third</button>
+            <button class="four">fourth</button>
         </div>
     `;
 }
+
+$(function(){
+    $('#button').click(function(){
+        if(!$('#iframe').length){
+            $('#iframeHolder').html('<iframe id="iframe" src="https://www.foxnews.com/" width="700" height="450"></iframe>');
+        }
+    });
+});
