cb3c54ebb6ff4bc31b96cd28b8827adcc2895f44
Phillip Temple <phillipcorteztemple@gmail.com>
2019-01-03 14:42:26 -0600

    button styles

--- src/Screens/Choices.js
+++ src/Screens/Choices.js
@@ -2,10 +2,10 @@ export default function Choices(){
     return `
         <div class="choices">
             <button class="History">Histroy</button>
+            <div id="iframeHolder"></div>
             <button class="underErasure">Under Erasure</button>
             <button class="three">third</button>
             <button class="four">fourth</button>
         </div>
     `;
 }
-
