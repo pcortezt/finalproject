f0a1cf1e53044e4612b573c88670e56e03316fde
Phillip Temple <phillipcorteztemple@gmail.com>
2019-01-20 17:21:04 -0600

    project is done

--- src/Screens/Changes3.js
+++ src/Screens/Changes3.js
@@ -3,8 +3,8 @@ export default function Changes3(){
     <div class="changes3">
         <button class="History">Ugly</button>
         <button class="underErasure">History Is Dead</button>
-        <button class="three">Brutalist Even More</button>
-        <button class="four"> Brutalist the Mostest</button>
+        <button class="three">Transparent</button>
+        <button class="four"> Rewrite the World</button>
     </div>
     `;
 }
